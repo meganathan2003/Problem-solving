c62545cd8b2cfd9fc3f2bad796e40ed528853d08
meganathan/2003 <meganathan212003@gmail.com>
2024-05-27 09:49:53 +0530

    Solved problems

---
+++ src/com/problems/easy/LargestAltitude.java
@@ -0,0 +1,30 @@
+package com.problems.easy;
+
+/**
+ * Below the code for find the largest
+ * Altitude in the trip
+ * Input: gain = [-5,1,5,0,-7]
+ * Output: 1
+ */
+public class LargestAltitude {
+
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+
+        for(int alti : gain){
+            currentAltitude += alti;
+            maxAltitude = Math.max(currentAltitude,maxAltitude);
+        }
+        return maxAltitude;
+    }
+    // TODO: Read and understand this sum clearly tomorrow
+
+    public static void main(String[] args) {
+        int[] gain = {-5, 1, 5, 0, -7};
+        // create a obj
+        LargestAltitude altitude = new LargestAltitude();
+        System.out.println(altitude.largestAltitude(gain));
+    }
+
+}
