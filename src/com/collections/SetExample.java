904fba36b3b18bc387f46f5eea249fa12e119ea3
meganathan/2003 <meganathan212003@gmail.com>
2024-06-04 15:11:58 +0530

    added new files

---
+++ src/com/collections/SetExample.java
@@ -0,0 +1,23 @@
+package com.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Below the code for learn the set and
+ * implement the code set is very powerful
+ * while manipulate the data it inherit the
+ * abstract class and Set interface
+ *
+ * @author Meganathan
+ */
+public class SetExample {
+
+    public static void main(String[] args) {
+
+        // import the set
+        Set<String> set = new HashSet<String>();
+        
+
+    }
+}
