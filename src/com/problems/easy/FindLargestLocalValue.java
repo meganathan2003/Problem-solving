6c5b2246d36a7fb53058974bf5552113d221f98b
unknown <meganathan212003@gmail.com>
2024-09-03 23:20:20 +0530

    Solved new sums

--- src/com/problems/easy/FindLargestLocalValue.java
+++ src/com/problems/easy/FindLargestLocalValue.java
@@ -8,16 +8,50 @@ package com.problems.easy;
  */
 public class FindLargestLocalValue {
 
-    // create a seprate method TODO:need to completed
-    // public int[][] largestLocal(int[][] grid) {
-    //     int n = grid.length;
-    //     int[][] maxLocal = new int[n - 2][n - 2];
+    // create a seprate method
+    public static int[][] generateMaxLocalMatrix(int[][] grid) {
+        int n = grid.length;  // size of the original grids
+        System.out.println(n);
+        int[][] maxLocal = new int[n - 2][n - 2];
 
-    //     //  Run the loop and get the largest number in the array
-    //     for (int i = 0; i < grid.length; i++) {
-    //         int largeNumber = 0;
-    //     }
+        // Loop through each possible 3x3 submatrix
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
 
-    // }
+                int maxVal = Math.max(
+                        Math.max(grid[i][j], grid[i][j + 1]),
+                        Math.max(grid[i][j + 2], grid[i + 1][j])
+                );
+                maxVal = Math.max(maxVal, Math.max(
+                        Math.max(grid[i + 1][j + 1], grid[i + 1][j + 2]),
+                        Math.max(grid[i + 2][j], grid[i + 2][j + 1])
+                ));
+                maxVal = Math.max(maxVal, grid[i + 2][j + 2]);
+
+                maxLocal[i][j] = maxVal;  // Store the max value in maxLocal
+            }
+        }
+
+        return maxLocal;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+            {9, 9, 8, 1},
+            {5, 6, 2, 6},
+            {8, 2, 6, 4},
+            {6, 2, 2, 2}
+        };
+
+        int[][] maxLocal = generateMaxLocalMatrix(grid);
+
+        // Print the result
+        for (int[] maxLocal1 : maxLocal) {
+            for (int j = 0; j < maxLocal1.length; j++) {
+                System.out.print(maxLocal1[j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
