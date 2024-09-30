a92bb8a675ed838bf62357c38550eacf5e15fc9a
unknown <meganathan212003@gmail.com>
2024-09-30 23:25:19 +0530

    Solve the leetcode sums

--- src/com/problems/easy/Arrays/SortFreqManner.java
+++ src/com/problems/easy/Arrays/SortFreqManner.java
@@ -3,14 +3,23 @@ package com.problems.easy.Arrays;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * Here the problems is to solve that
+ * you are given the array you need
+ * sort that based one frequency if
+ * both number are same frequency sort
+ * that increasing order
+ *
+ * @author meganathan
+ */
 public class SortFreqManner {
 
-    // create a seprate method
+    // create a separate method
     public static void sortFreqManner(Integer[] arr) {
 
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         StringBuilder builder = new StringBuilder();
-        
+
         for (int j : arr) {
             frequencyMap.put(j, frequencyMap.getOrDefault(j, 0) + 1);
         }
@@ -23,8 +32,7 @@ public class SortFreqManner {
             System.out.println("Arr" + b);
             int freqA = frequencyMap.get(a); // Frequency of element a
             int freqB = frequencyMap.get(b); // Frequency of element b
-            
-            
+
 
             System.out.println("Freq" + freqA);
             System.out.println("Freq" + freqB);
@@ -33,8 +41,8 @@ public class SortFreqManner {
                 // If frequencies are equal, compare the values directly (for ascending order)
                 return a - b;
             }
-            
-            
+
+
             // Otherwise, compare based on frequency (for ascending frequency)
             return freqA - freqB;
         });
