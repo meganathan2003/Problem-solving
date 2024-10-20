076bb5bbacb169b6c710d24ffb2758e19bf9cff2
unknown <meganathan212003@gmail.com>
2024-10-20 18:31:26 +0530

    Solved problem

---
+++ src/com/problems/easy/Arrays/ArrayPairSum.java
@@ -0,0 +1,27 @@
+package com.problems.easy.Arrays;
+
+/**
+ * Given an integer array nums of 2n integers,
+ * group these integers into n pairs (a1, b1), (a2, b2),
+ * ..., (an, bn) such that the sum of min(ai, bi) for
+ * all i is maximized. Return the maximized sum.
+ *
+ * @author Meganathan
+ */
+public class ArrayPairSum {
+
+    public int arrayPairSum(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
