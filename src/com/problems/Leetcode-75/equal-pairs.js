197a97ef8f38ca39deb4349d54076ec4cf42e34f
unknown <meganathan212003@gmail.com>
2024-11-27 13:43:44 +0530

    complete the code

---
+++ src/com/problems/Leetcode-75/equal-pairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function (grid) {
+
+    if (grid.length == 0 || grid[0].length == 0) {
+        return 0;
+    }
+
+    
+
+
+
+
+
+}; 
