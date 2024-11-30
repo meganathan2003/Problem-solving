f006831aafc68fd1e8c4a17e73492a34b18d52df
unknown <meganathan212003@gmail.com>
2024-11-30 21:47:12 +0530

    complete problems solving

--- src/com/problems/Leetcode-75/equal-pairs.js
+++ src/com/problems/Leetcode-75/equal-pairs.js
@@ -3,15 +3,31 @@
  * @return {number}
  */
 var equalPairs = function (grid) {
+    const n = grid.length;
+    const rowMap = new Map();
 
-    if (grid.length == 0 || grid[0].length == 0) {
-        return 0;
+    for (const row of grid) {
+        const rowKey = row.join(",");
+        rowMap.set(rowKey, (rowMap.get(rowKey) || 0) + 1);
     }
 
-    
+    let count = 0;
 
+    for (let col = 0; col < n; col++) {
+        const colKey = [];
+        for (let row = 0; row < n; row++) {
+            colKey.push(grid[row][col]);
+        }
+        const colString = colKey.join(",");
+        count += rowMap.get(colString) || 0;
+    }
 
+    return count;
+}
 
-
-
-}; 
+const grid = [
+    [3, 2, 1],
+    [1, 7, 6],
+    [2, 7, 7]
+];
+console.log(equalPairs(grid)); 
