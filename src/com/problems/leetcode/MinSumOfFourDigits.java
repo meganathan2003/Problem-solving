e29c4861ba9f5c15889e4cd8c9acb8ab7071efcc
unknown <meganathan212003@gmail.com>
2024-12-16 21:59:40 +0530

    Solved Today sums

---
+++ src/com/problems/leetcode/MinSumOfFourDigits.java
@@ -0,0 +1,21 @@
+package com.problems.leetcode;
+
+public class MinSumOfFourDigits {
+
+    //Create a static method
+    public static int minimumSum(int num) {
+
+
+//        validation
+        if (num < 0) {
+            return 0;
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
