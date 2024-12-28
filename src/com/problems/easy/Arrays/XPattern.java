93ecdcf8de26efd3cfce30ed068ce0f20a8ba567
meganathan <meganathan212003@gmail.com>
2024-12-28 20:58:47 +0530

    solved sums

--- src/com/problems/easy/Arrays/XPattern.java
+++ src/com/problems/easy/Arrays/XPattern.java
@@ -15,7 +15,7 @@ public class XPattern {
                 else
                     System.out.print(".");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
     }
 }
